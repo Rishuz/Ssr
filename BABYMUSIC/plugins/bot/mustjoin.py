044d82be26f7848761cddfda9fddd91faed811b6
SANATANIxTECH <sanatanixtech@gmail.com>
2025-01-22 20:49:32 +0530

    New!

--- BABYMUSIC/plugins/bot/mustjoin.py
+++ BABYMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from BABYMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "ProBotts"
+MUST_JOIN = "SANATANI_TECH"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
