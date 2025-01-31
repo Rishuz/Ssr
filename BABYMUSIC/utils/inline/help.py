c031daea89a2266bc3bc25e8cb89bb5ef41ebc00
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2025-01-31 10:23:40 +0530

    Update help.py

--- BABYMUSIC/utils/inline/help.py
+++ BABYMUSIC/utils/inline/help.py
@@ -1,7 +1,5 @@
 from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from BABYMUSIC import app
-from BABYMUSIC.utils.inline.help import help_back_markup, private_help_panel
 
 
 def help_pannel(_, START: Union[bool, int] = None):
